de7a90acb675840a6b341169738778ce7a50d631
kissyoudyb <danyubin1982@163.com>
2026-02-09 19:23:52 +0800

    微调了Qwen2.5-1.5b-instruct模型，成功了，微调效果出来了，蛋蛋妹优秀哈哈，使用了医疗数据集

--- L2/demo_08/download-datasets.py
+++ L2/demo_08/download-datasets.py
@@ -1,5 +1,8 @@
 #数据集下载
 # pip install datasets oss2 addict "datasets<3.0.0"
+#数据集下载
+# pip install datasets oss2 addict "datasets<3.0.0"
 from modelscope.msdatasets import MsDataset
-ds =  MsDataset.load('w10442005/ruozhiba_qa', subset_name='default', split='train',cache_dir="/root/datasets")
+# ds =  MsDataset.load('w10442005/ruozhiba_qa', subset_name='default', split='train',cache_dir="/root/datasets")
+ds =  MsDataset.load('xiaofengalg/Chinese-medical-dialogue', subset_name='default', split='train',cache_dir="/root/datasets")
 #您可按需配置 subset_name、split，参照“快速使用”示例代码
--- L2/demo_08/download-llms.py
+++ L2/demo_08/download-llms.py
@@ -1,2 +1,3 @@
 from modelscope import snapshot_download
-model_dir = snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',cache_dir="/root/llms")
+# model_dir = snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',cache_dir="/root/llms")
+model_dir = snapshot_download('Qwen/Qwen2.5-1.5B-Instruct',cache_dir="/root/llms")
